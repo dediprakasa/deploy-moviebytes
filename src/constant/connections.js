47d94c55b775c456e24469cea731590ff2c2dbf8
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2021-04-11 23:42:49 +0700

    Fix https

--- src/constant/connections.js
+++ src/constant/connections.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const callOmdbApi = axios.create({
-  baseURL: `http://www.omdbapi.com/?apikey=5f3e1158&`,
+  baseURL: `https://www.omdbapi.com/?apikey=5f3e1158&`,
 });
 
 export default callOmdbApi;
